fe37390d0db51fc04a0e629e0609699a5439340c
Rajeev Ranjan Kumar <Rajeevrk9901@gmail.com>
2022-07-16 13:47:55 +0530

    UseState inc dec

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -3,18 +3,34 @@ import React, { useState } from "react";
 function Counter() {
   const [counter, setCounter] = useState(0);
 
-  const countHandler = () => {
+  const increaseCountHandler = () => {
     setCounter(counter + 1);
+  };
+
+  const decreaseCountHandler = () => {
+    setCounter(counter - 1);
   };
 
   return (
     <>
       <h1>{counter}</h1>
+
       <button
-        style={{ padding: "12px", background: "grey", color: "white" }}
-        onClick={countHandler}
+        style={{
+          padding: "12px",
+          marginRight: "2px",
+          background: "grey",
+          color: "white",
+        }}
+        onClick={increaseCountHandler}
       >
         Increase
+      </button>
+      <button
+        style={{ padding: "12px", background: "grey", color: "white" }}
+        onClick={decreaseCountHandler}
+      >
+        Decrease
       </button>
     </>
   );
